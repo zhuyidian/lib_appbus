112faff2e7591bfbe4952c2a75e36efe5c0c27e1
zhuyidian <zhuyidian@coocaa.com>
2021-08-10 17:35:10 +0800

    actiON:YOUHUA

--- lib_appbus/AppBusSdk/src/main/java/com/coocaa/appbus/AppBus.java
+++ lib_appbus/AppBusSdk/src/main/java/com/coocaa/appbus/AppBus.java
@@ -54,9 +54,20 @@ public class AppBus {
 
     public void init(Context context){
         this.mContext = context.getApplicationContext();
+        LogUtil.d("service","init: mContext="+mContext);
     }
 
     public void destroy(){
+        try {
+            LogUtil.d("service","destroy: mNotifyAidl="+mNotifyAidl);
+            if (mNotifyAidl != null) {
+                mContext.unbindService(mNotifyConnection);
+                mNotifyAidl = null;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            LogUtil.d("service","destroy: e="+e);
+        }
         mContext = null;
     }
 
@@ -144,11 +155,11 @@ public class AppBus {
                     LogUtil.d("service","[Notify] update: mRemoteCallbacks size="+mRemoteCallbacks.getRegisteredCallbackCount());
                     //这里需要判断控制中心进程有灭有起来?如果没有起来，那么要bindService远程拉起进程，将数据传递过去
                     //检查NotifyService，目前检查mRemoteCallbacks来确定控制中心有没有连接
-                    //if(mRemoteCallbacks.getRegisteredCallbackCount()<=0){
+                    if(mRemoteCallbacks.getRegisteredCallbackCount()<=0){
                         //添加数据到队列
                         queue.offer(appInfoList);
                         bindNotifyService(mContext);
-                    //}
+                    }
 
                     final int N = mRemoteCallbacks.beginBroadcast();
                     for (int i = 0; i < N; i++) {
